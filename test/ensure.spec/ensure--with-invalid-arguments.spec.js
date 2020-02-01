f9c2b221d94002e439835f0077f00deeaf993979
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-02-01 10:10:59 +0100

    Add a test case where the test-func is missing

--- test/ensure.spec/ensure--with-invalid-arguments.spec.js
+++ test/ensure.spec/ensure--with-invalid-arguments.spec.js
@@ -1,9 +1,16 @@
 const { ensure } = require("../../lib/ensure");
 const { should_throw, equals } = require('../helper');
-/**
- * @type {Error}
- */
-const param2_notFuncError = should_throw(() => ensure(2, 3));
 
+// It should throw when the second argument is a function
+ensure(
+    should_throw(() => ensure(2, 3)).message,
+    equals,
+    "3 is not a function, but a number"
+);
 
-ensure(param2_notFuncError.message, equals, "3 is not a function, but a number");
+// The following test prevents test cases were a test-function was forgotten (incorrect definition or otherwise)
+ensure(
+    should_throw(() => ensure(true, undefined)).message,
+    equals,
+    "Expecting the second parameter to be a function, but was falsy"
+);
