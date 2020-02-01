d7cf09de4cdc229f33eadc030813d847fe192a3e
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-02-01 08:23:33 +0100

    Add tests for basic functions

--- test/tests.spec.js
+++ test/tests.spec.js
@@ -1,3 +1,5 @@
 // All the tests
 require('./tests--is_truthy.spec.js');
-require('./tests--is_falsy.spec')
+require('./tests--is_falsy.spec');
+require('./tests--is_not.spec');
+require('./tests--is.spec');
